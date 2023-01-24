0085506acf21ec1a8f9c3ec1fe3ec7bdedf7d419
SergeyMorin <serega-morin@yandex.ru>
2023-01-24 14:54:51 +0300

    Добавил вторую версию программы в ветку program

--- program/Program.cs
+++ program/Program.cs
@@ -6,29 +6,30 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
-//string[] array= new string[]{"Hello","2","world",":-)"};
-string[] array = new string[] { "Hello", "world" };
+ string[] array= new string[]{"2","Hello","2","world",":-)"};
+// string[] array = new string[] { "Hello", "world" };
 PrintArray(Program(array));
 
 void PrintArray(string[] array1)
 {
     for (int i = 0; i < array1.Length; i++)
     {
-        Console.Write(array1[i] + " ");
+        Console.Write(array1[i] + "1 ");
     }
 }
 
-string[] Program(string[] array1)
+string[] Program( string[] array1)
 {
     string[] array2 = new string[1];
-    int sizeArray2 = array1.Length;
+    array2[0]="there are no strings in the array whose length is less than or equal to 3 characters";
+    int sizeArray2=1;
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
+            array2[sizeArray2-1] = array1[i];
             sizeArray2++;
-            Array.Resize(ref array2, sizeArray2);
-            array2[i] = array1[i];
+            Array.Resize(ref array2, sizeArray2); 
         }
     }
     return array2;
