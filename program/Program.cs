9083f8e40ace12e9b18010b8be126f83b10b9c07
SergeyMorin <serega-morin@yandex.ru>
2023-01-24 16:42:45 +0300

    Добавил третью версию программы в ветку program

--- program/Program.cs
+++ program/Program.cs
@@ -6,31 +6,55 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
- string[] array= new string[]{"2","Hello","2","world",":-)"};
-// string[] array = new string[] { "Hello", "world" };
-PrintArray(Program(array));
 
-void PrintArray(string[] array1)
+string[] arrayEx1 = new string[] { "Hello", "world", "2", ":-)" }; //Пример массива 1
+string[] arrayEx2 = new string[] { "1234", "1567", "-2", "computer since" }; //Пример массива 2
+string[] arrayEx3 = new string[] { "Russia", "Denmark", "Kazan" }; //Пример массива 3
+
+Console.Write("["); //Пример 1 Вывод результата в консоль
+PrintArray(arrayEx1);
+Console.Write(" ] → ");
+Console.Write("[");
+PrintArray(Program(arrayEx1));
+Console.WriteLine(" ]");
+
+Console.Write("["); //Пример 2 Вывод результата в консоль
+PrintArray(arrayEx2);
+Console.Write(" ] → ");
+Console.Write("[");
+PrintArray(Program(arrayEx2));
+Console.WriteLine(" ]");
+
+Console.Write("["); //Пример 3 Вывод результата в консоль
+PrintArray(arrayEx3);
+Console.Write(" ] → ");
+Console.Write("[");
+PrintArray(Program(arrayEx3));
+Console.WriteLine(" ]");
+
+void PrintArray(string[] array) // вывод массива в консоль
 {
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array1[i] + "1 ");
+        Console.Write(" {" + array[i] + "}");
     }
 }
 
-string[] Program( string[] array1)
+// Программа, которая из имеющегося массива строк формирует новый массив
+// из строк, длина которых меньше, либо равна 3 символам.
+string[] Program(string[] imputArray)
 {
-    string[] array2 = new string[1];
-    array2[0]="there are no strings in the array whose length is less than or equal to 3 characters";
-    int sizeArray2=1;
-    for (int i = 0; i < array1.Length; i++)
+    string[] outArray = new string[1];
+    outArray[0] = "there are no strings in the array whose length is less than or equal to 3 characters";
+    int sizeOutArray = 0;
+    for (int i = 0; i < imputArray.Length; i++)
     {
-        if (array1[i].Length <= 3)
+        if (imputArray[i].Length <= 3)
         {
-            array2[sizeArray2-1] = array1[i];
-            sizeArray2++;
-            Array.Resize(ref array2, sizeArray2); 
+            sizeOutArray++;
+            Array.Resize(ref outArray, sizeOutArray);
+            outArray[sizeOutArray - 1] = imputArray[i];
         }
     }
-    return array2;
+    return outArray;
 }
